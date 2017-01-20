e02bc21987c868bb41f93d41cd49c6dea7e4fab9
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-20 20:35:39 +0100

    add question model
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ app/Question.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+    protected $fillable = [
+        'description', 'weight'
+    ]; 
+
+    public static function validator($input){
+
+        $rules = array(
+            'description' => 'required|description',
+            'weight' => 'required|weight'
+        );
+
+        return Validator::make($input,$rules);
+    };
+
+    public function category(){
+	    return $this->hasOne(Category::class);
+	}
+
+    public function answers(){
+	    return $this->hasMany(Answer::class);
+	}
+}
