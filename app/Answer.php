96618805c26c519443e2e2add5e5905d0a1c2f26
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-20 20:35:11 +0100

    add answers model
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ app/Answer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    //
+    protected $fillable = [
+        'description', 'isCorrect'
+    ]; 
+
+    public static function validator($input){
+
+        $rules = array(
+            'description' => 'required|description',
+            'isCorrect' => 'required|isCorrect'
+        );
+
+        return Validator::make($input,$rules);
+    };
+
+    public function question(){
+	    return $this->hasOne(Question::class);
+	}
+}
