575dcfdcd491acf2cb75528cfd5e4e36b72a7a75
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-23 06:21:03 +0100

    add interv controller
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- app/Http/Controllers/IntervController.php
+++ app/Http/Controllers/IntervController.php
@@ -16,26 +16,25 @@ class IntervController extends Controller
     //
       public function store(Request $request)
     {
-       $Interv = new Interv;
-    $Interv->first_name = $request->first_name;
-    $Interv->last_name = $request->last_name;
-     $Interv->email = $request->email;
-     $Interv->tests = $request->tests;
-     $Interv->unit = $request->unit;
-       $Interv->password = $request->password;
-   $Interv->save();
+        $Interv = new Interv;
+        
+        $Interv->first_name = $request->first_name;
+        $Interv->last_name = $request->last_name;
+        $Interv->email = $request->email;
+        $Interv->tests = $request->tests;
+        $Interv->unit = $request->unit;
+        $Interv->password = $request->password;
+        $Interv->save();
 
-
-            return Redirect::to('admin/profiling')
-                ->with('success','Interv has been saved.');
+        return Redirect::to('admin/profiling')->with('success','Interv has been saved.');
 
         
     }
 
      public function show()
     {
-            $Intervs = Interv::Paginate(3);
-        return view('admin.intervs', compact('Intervs'));
+        $intervs = Interv::Paginate(3);
+        return view('admin.intervs', compact('intervs'));
     }
 
 
