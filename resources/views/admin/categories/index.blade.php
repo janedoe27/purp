e8ffbb34fdb19b5f3321609da70c1b72ba9026ef
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-25 04:50:53 +0100

    update cateory view
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.dashboard')
-
+@section('new_resource')
+<button class="btn btn-success" data-toggle="modal" data-target="#formModal">New Category</button>
+@endsection
 @section('content')
 <div class="box">
             <div class="box-header">
@@ -39,4 +41,38 @@
       </div>
       <!-- /.row -->
   
+      <!-- Modal -->
+      <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">New Category</h4>
+            </div>
+            <div class="modal-body">
+              <form role="form" action="/app/categories/new" method="post">
+                <div class="form-group">
+                  <label>Name</label>
+                  <input type="text" name="name" class="form-control" placeholder="Enter ..." ">
+                </div>
+                  <div class="form-group">
+                  <label>Description</label>
+                  <textarea name="description" class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  @endsection
+  @section('local_js')
+  <script type="text/javascript">
+    $(document).ready(function() {
+
+    })
+  </script>
   @endsection
