502d2978cb7a0351aa1ce0daf2254c99650c9016
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-28 16:30:54 +0100

    update app view
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,45 +5,40 @@
  <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-       @foreach ($questtests->getCollection() as $questtest)
-      <h1 class="header center orange-text">QUESTION {{$questtest -> id}} </h1>
+       @foreach ($questions->getCollection() as $question)
+      <h1 class="header center orange-text">QUESTION {{$question -> id}} </h1>
       <div class="row center" id="app">
       
         <h3 class="header col s12 light">
           
-          {{$questtest -> question}}
+          {{$question -> description}}
           
           </h3>
    
       </div>
       
-      <br><br>
         <div class="row center">
           <form action="#">
-    <p>
-      <input name="answer1" type="radio" id="test1" />
-      <label for="test1"> {{$questtest-> answer1}}</label>
-    </p>
-    <p>
-      <input name="group1" type="radio" id="test2" />
-      <label for="test2">{{$questtest-> correct}}</label>
-    </p>
-    <p>
-      <input  name="group1" type="radio" id="test3"  />
-      <label for="test3">{{$questtest-> answer3}}</label>
-    </p>
-  </form>
-   @endforeach
+            @foreach ($question->answers as $answer)
+            <p>
+              <input name="answer" data-cbt-value="{{$answer->id}}" type="radio" id="test" />
+              <label for="test"> {{$answer-> description}}</label>
+            </p>
+            @endforeach
+            <button type="submit" class="btn btn-success">Save</button>
+          </form>
+        </div>
+      @endforeach
      </div>
-     
-      <div class="row center">
-        <ul class="pagination">
-        {{ $questtests->links()}}
-        
-          <a class="waves-effect waves- orange darken-4 btn" href="">NEXT</a>
-      </u/>
-    </div>
+        <div class="row center">
+          {{$questions->links()}}
+        </div>
   </div>
-     
-
+  @endsection
+  @section('local_js')
+  <script type="text/javascript">
+    $(function(){
+      $('.disabled').addClass('hidden').hide();
+    })
+  </script>
   @endsection
