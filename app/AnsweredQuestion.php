d98310794fb6fb7dbd00e911db918ec69188090c
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-02 04:49:59 +0100

    update answered question model
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- app/AnsweredQuestion.php
+++ app/AnsweredQuestion.php
@@ -4,12 +4,36 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class AnsweredQuestion extends Model
 {
+    use Uuids;
+    
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
     //
     protected $fillable = [
         'question_id', 'answer_id', 'interviewee_id'
     ]; 
+
+    protected $hidden = array('created_at', 'updated_at');
+
+    public static function validator($input){
+
+        $rules = array(
+            'question_id' => 'required|question_id',
+            'answer_id' => 'required|answer_id',
+            'interviewee_id' => 'required|interviewee_id'
+        );
+
+        return Validator::make($input,$rules);
+    }
+
 
     public function question(){
 	    return $this->hasOne(Question::class);
