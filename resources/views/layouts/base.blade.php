def6a6b0cb82de6bf5b7af9da9e01ef24676b638
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-03 04:17:45 +0100

    update base project
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,8 +20,22 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+    <style>
+       body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1 0 auto;
+  }
+      
+
+    </style>
 </head>
 <body>
+  <header>
   <nav class="orange darken-4" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="{{url('/') }}" class="brand-logo">{{ config('app.name', 'GTB Exam') }}</a>
       <ul class="right hide-on-med-and-down">
@@ -30,7 +44,7 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
-        <li><a href="{{url('/app')}}">HI {{ Auth::user()->first_name }}</a></li>
+        <li><a href="{{url('/app')}}">Hi {{ Auth::user()->first_name }}</a></li>
                                     <li>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
@@ -51,20 +65,27 @@
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
   </nav>
+  </header>
+  <main class="wrapper">
 
+  @if (session('teststatus'))
+  <div class="alert alert-success">
+      {{ session('teststatus') }}
+  </div>
+  @endif
   @yield('content')
-
+  </main>
   <footer class="page-footer orange darken-4">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">GTBANK SL</h5>
-          <p class="grey-text text-lighten-4">Wouldn't You Rather Bank With US?.</p>
+          <p class="grey-text text-lighten-4">Wouldn't You Rather Bank With Us?.</p>
 
 
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
+          <h5 class="white-text">Learn</h5>
           <ul>
             <li><a class="white-text" href="#!">Link 1</a></li>
             <li><a class="white-text" href="#!">Link 2</a></li>
@@ -73,7 +94,7 @@
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
+          <h5 class="white-text">Social</h5>
           <ul>
             <li><a class="white-text" href="#!">Link 1</a></li>
             <li><a class="white-text" href="#!">Link 2</a></li>
