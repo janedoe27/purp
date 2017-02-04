f24f8cd113039276b8af7d4109b0d8516ccb8185
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-04 08:42:58 +0100

    update kernel.php add observer
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,5 +55,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'staff' => \App\Http\Middleware\StaffMiddleware::class,
     ];
 }
