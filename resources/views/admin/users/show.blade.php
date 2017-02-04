9687a07d6fa83d29737a3586c3dde5733f8844b1
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-04 11:50:36 +0100

    add show user view
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ resources/views/admin/users/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        </div>
+        <!-- /.col -->
+      </div>
+      </section>
+      
+
+  @endsection
