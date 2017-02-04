65349ddd039ef8e346daaddf8ea34ce9dc1ffaff
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-04 08:45:47 +0100

    update register view
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,30 +61,14 @@
       </div>
       </div>
 
-      <div class="form-group">
-
-      <p class="center">
-      <input value="1" type="checkbox"  id="is_admin" name="is_admin" />
-      <label for="is_admin">Staff</label>
-    </p>
-  
-                  <div class="checkbox center" >
-                    <label>
-                      <input  value="1" type="checkbox" name="is_admin">
-                      Staff
-                    </label>
-                  </div>
-            </div>
-
       <div class="row center">
-      <button class="btn waves-effect waves- orange darken-4" type="submit">START
-  </button>
+      <button class="btn waves-effect waves- orange darken-4" type="submit">START</button>
       </div>
     </form>
       
       <br><br>
       <div class="row center">
-        <h5>Please note that once you click the Login the Exams starts</h5>
+        <h6>Please note that once you click the Login the Exams starts</h6>
       </div>
     </div>
   </div>
