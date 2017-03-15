809e3c4c30730350cd4cc71e899e254edee6b9d4
janedoe27 <douglascleopatra@gmail.com>
2017-03-15 13:12:32 +0000

    Update app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://fierce-lake-95225.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
